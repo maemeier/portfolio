49ed601d05a99c7d503fd08f862f3eaaa7db0629
Mae Kangsadan <maemeier@live.com>
2020-01-06 22:58:24 +0100

    link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
 import { FaAlignRight } from "react-icons/fa";
 
-export default class Navbar1 extends Component {
+export default class Navbar extends Component {
   state = {
     isOpen: false
   };
@@ -12,19 +11,32 @@ export default class Navbar1 extends Component {
   };
   render() {
     return (
-      <div className="navbar">
-        <ul>
-          <li className="link">
-            <Link to="/aboutMe">AboutMe</Link>{" "}
-          </li>
-          <li className="link">
-            <Link to="/coding">Projects</Link>{" "}
-          </li>
-          <li className="link">
-            <Link to="/">Home</Link>{" "}
-          </li>
-        </ul>
-      </div>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/coding">Coding</Link>
+            </li>
+            <li>
+              <Link to="/aboutMe">About Me</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
